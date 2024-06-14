910c3ae277d4e89439dddfd226defe300d7a9477
Yi Xu <yiyix@chromium.org>
2024-06-14 14:31:23 -0700

    Move the-canvas-element to new test generator
    Bug: 1275750
    
    Change-Id: Iaac71660ddf5dae338e62454fcca5868cf10cf02

--- html/canvas/offscreen/canvas-host/2d.canvas.host.size.attributes.reflect.setidl.worker.js
+++ html/canvas/offscreen/canvas-host/2d.canvas.host.size.attributes.reflect.setidl.worker.js
@@ -20,6 +20,8 @@ t.step(function() {
   canvas.height = 60;
   _assertSame(canvas.width, 120, "canvas.width", "120");
   _assertSame(canvas.height, 60, "canvas.height", "60");
+  _assertSame(canvas.getAttribute('width'), '120', "canvas.getAttribute('width')", "'120'");
+  _assertSame(canvas.getAttribute('height'), '60', "canvas.getAttribute('height')", "'60'");
   t.done();
 });
 done();
--- html/canvas/offscreen/canvas-host/2d.canvas.host.size.attributes.reflect.setidlzero.worker.js
+++ html/canvas/offscreen/canvas-host/2d.canvas.host.size.attributes.reflect.setidlzero.worker.js
@@ -18,6 +18,8 @@ t.step(function() {
 
   canvas.width = 0;
   canvas.height = 0;
+  _assertSame(canvas.getAttribute('width'), '0', "canvas.getAttribute('width')", "'0'");
+  _assertSame(canvas.getAttribute('height'), '0', "canvas.getAttribute('height')", "'0'");
   _assertSame(canvas.width, 0, "canvas.width", "0");
   _assertSame(canvas.height, 0, "canvas.height", "0");
   t.done();
